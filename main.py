9ebd7fb22b262ec52eaba69b8fbd2359391cc7d6
Mateusz <matikrajek42@gmail.com>
2024-04-19 14:21:44 +0200

    .

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def main():
+def main(args, kwargs)->None:
+    """główna funkcja programu
+
+    Args:
+        args (dit(int)): argumenty
+        kwargs (None): nwm
+    """
     print("123")
 
 if __name__ == "__main__":
